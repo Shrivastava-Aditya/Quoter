955205c3e6136d821ce40b5742cdc02be9a0d3f0
Shrivastava-Aditya <Aditya@github.com>
2022-03-23 01:28:45 +0530

    configuring for heroku

--- quoter/quoter/settings.py
+++ quoter/quoter/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +27,8 @@ SECRET_KEY = 'django-insecure-35)y^gbfxe73s3#s#z04b#&27u_cm+kyof33x1r)=66k^(r&kf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+django_heroku.settings(locals())
 
 # Application definition
 
